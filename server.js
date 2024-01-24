aabbb1ef60e47e8c1212069a0a320ea0c7b035d2
butch888 <never_man@mail.ru>
2024-01-24 23:13:38 +0300

    i husky

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.get('/', (req, res) => {
     res.send('Hello Maks');
 });
-let x = 10;
+
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server started on port: ${PORT}`);

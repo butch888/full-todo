fd4e3ddde1317a547d5159857030bd55f134d893
butch888 <never_man@mail.ru>
2024-02-10 18:21:37 +0300

    Add routers

---
+++ rest/api/getTasks.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const pool = require('../../bd');
+
+const getTasksRouter = express.Router();
+
+// получить все записи
+// eslint-disable-next-line consistent-return
+getTasksRouter.get('/', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT * FROM tasks');
+        res.send(result.rows);
+        return result.rows;
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Server error');
+    }
+});
+
+module.exports = getTasksRouter;
--- server.js
+++ server.js
@@ -1,12 +1,32 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello Maks');
-});
+const getTasksRouter = require('./rest/api/getTasks');
+const addTaskRouter = require('./rest/api/addTask');
+const editTaskRouter = require('./rest/api/editTask');
+const isdoneTaskRouter = require('./rest/api/isdoneTask');
+const delTaskRouter = require('./rest/api/delTask');
 
-const PORT = 3000;
+app.use(cors());
+
+// получить все записи
+app.use('/', getTasksRouter);
+
+// добавить задачу
+app.use('/', addTaskRouter);
+
+// редактировать текст задачи по id
+app.use('/', editTaskRouter);
+
+// отметить задачу выполненной по id
+app.use('/', isdoneTaskRouter);
+
+// редактировать текст задачи по id
+app.use('/', delTaskRouter);
+
+const PORT = 3005;
 app.listen(PORT, () => {
     console.log(`Server started on port: ${PORT}`);
 });
